7eaeab0ed33332d313a8578b8801a3f461b5481c
MrBirdatGit <446788720@qq.com>
2017-07-11 10:31:04 +0800

    修改

--- Grunt/dp/cf167b_概率dp_注意trick.cc
+++ Grunt/dp/cf167b_概率dp_注意trick.cc
@@ -1,3 +1,10 @@
+//有很多比赛, 每场送包或奖品，包的容量是a[i], 可以装a[i]个奖品
+//每场比赛夺冠概率已知为p[i]
+//问有多少概率达到目标夺冠次数并能带回所有奖品
+//比较水的概率dp
+//但是要注意两个trick:
+//一是参赛顺序可以自己调整, 所以要贪心地先参加送包的比赛
+//二是k可能大于n
 #include <bits/stdc++.h>
 using namespace std;
 double dp[205][205][205];
@@ -10,6 +17,7 @@ bool cmp(node a, node b){
 int n, l, k;
 int main(){
 	scanf("%d%d%d", &n, &l, &k);
+	k=min(k,n);//注意trick
 	for(int i=1;i<=n;i++)scanf("%d", &rec[i].p);
 	for(int i=1;i<=n;i++)scanf("%d", &rec[i].a);
 	sort(rec+1, rec+1+n, cmp);
@@ -17,7 +25,6 @@ int main(){
 	dp[0][k][0]=1;
 	for(int i=1;i<=n;i++){
 		for(int j=0;j<=n;j++){
-		//	for(int k=0;k<=n;k++){
 			for(int k=0;k<=n;k++){
 				if(rec[i].a==-1){
 					if(j>0){
@@ -31,17 +38,11 @@ int main(){
 				else {
 					int to=min(n, j+rec[i].a);
 					dp[i][to][k+1]+=dp[i-1][j][k]*rec[i].p/100;
-					if(dp[i-1][j][k]>0.5){
-						cout<<i<<' '<<j<<' '<<k<<endl;
-						cout<<i<<' '<<to<<' '<<k+1<<' '<<dp[i][to][k+1]<<endl;
-						return 0;
-					}
 					dp[i][j][k]+=dp[i-1][j][k]*(100-rec[i].p)/100;
 				}
 			}
 		}
 	}
-	printf("%.8f\n", dp[1][5][1]);
 	double ans=0;
 	for(int j=0;j<=n;j++){
 		for(int k=0;k<=n;k++){
